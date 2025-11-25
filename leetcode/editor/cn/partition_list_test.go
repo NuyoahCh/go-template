f99424ffeedb75b373472c1ddbed6944470a44e1
Morning <springoflotus@gmail.com>
2025-11-25 14:58:29 +0800

    feat：分隔链表

--- leetcode/editor/cn/partition_list_test.go
+++ leetcode/editor/cn/partition_list_test.go
@@ -9,7 +9,6 @@ package leetcode_solutions
 
 import "testing"
 
-
 // @lc code=start
 /**
  * Definition for singly-linked list.
@@ -19,16 +18,37 @@ import "testing"
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
-    
+	// 初始化
+	dummy1 := &ListNode{-1, nil}
+	dummy2 := &ListNode{-1, nil}
+	p := head
+	p1, p2 := dummy1, dummy2
+	for p != nil {
+		if p.Val < x {
+			p1.Next = p
+			p1 = p1.Next
+		} else {
+			p2.Next = p
+			p2 = p2.Next
+		}
+		// 防止成环
+		temp := p.Next
+		p.Next = nil
+		p = temp
+	}
+	p1.Next = dummy2.Next
+	return dummy1.Next
 }
+
 // @lc code=end
 
 func TestPartitionList(t *testing.T) {
 	// your test code here
-	
+	list1 := CreateHead([]int{1, 4, 3, 2, 5, 2})
+	x := 3
+	res := partition(list1, x)
+	PrintList(res)
 }
-
-
 
 /*
 // @lcpr case=start
@@ -39,5 +59,4 @@ func TestPartitionList(t *testing.T) {
 // [2,1]\n2\n
 // @lcpr case=end
 
- */
-
+*/
