c37ff74099ecf2871594b991a80bddc75f2f19bb
Morning <springoflotus@gmail.com>
2025-11-27 13:45:53 +0800

    opt：完善项目代码

--- leetcode/editor/cn/search_in_rotated_sorted_array_test.go
+++ leetcode/editor/cn/search_in_rotated_sorted_array_test.go
@@ -14,37 +14,37 @@ import (
 
 // @lc code=start
 func search(nums []int, target int) int {
-	// 左右都闭的搜索区间
-	left, right := 0, len(nums)-1
-	// 因为是闭区间，所以结束条件为 left > right
-	for left <= right {
-		mid := left + (right-left)/2
-		// 首先检查 nums[mid]，是否找到 target
-		if nums[mid] == target {
-			return mid
-		}
-		if nums[mid] >= nums[left] {
-			// mid 落在断崖左边，此时 nums[left..mid] 有序
-			if target >= nums[left] && target < nums[mid] {
-				// target 落在 [left..mid-1] 中
-				right = mid - 1
-			} else {
-				// target 落在 [mid+1..right] 中
-				left = mid + 1
-			}
-		} else {
-			// mid 落在断崖右边，此时 nums[mid..right] 有序
-			if target <= nums[right] && target > nums[mid] {
-				// target 落在 [mid+1..right] 中
-				left = mid + 1
-			} else {
-				// target 落在 [left..mid-1] 中
-				right = mid - 1
-			}
-		}
-	}
-	// while 结束还没找到，说明 target 不存在
-	return -1
+    // 左右都闭的搜索区间
+    left, right := 0, len(nums) - 1
+    // 因为是闭区间，所以结束条件为 left > right
+    for left <= right {
+        mid := left + (right - left) / 2
+        // 首先检查 nums[mid]，是否找到 target
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[mid] >= nums[left] {
+            // mid 落在断崖左边，此时 nums[left..mid] 有序
+            if target >= nums[left] && target < nums[mid] {
+                // target 落在 [left..mid-1] 中
+                right = mid - 1
+            } else {
+                // target 落在 [mid+1..right] 中
+                left = mid + 1
+            }
+        } else {
+            // mid 落在断崖右边，此时 nums[mid..right] 有序
+            if target <= nums[right] && target > nums[mid] {
+                // target 落在 [mid+1..right] 中
+                left = mid + 1
+            } else {
+                // target 落在 [left..mid-1] 中
+                right = mid - 1
+            }
+        }
+    }
+    // while 结束还没找到，说明 target 不存在
+    return -1
 }
 
 // @lc code=end
